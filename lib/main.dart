4e3f15af20a2364ceaa99287f6ee2efb879a56c1
NiMo- <mo5-98@hotmail.com>
2020-04-07 12:19:02 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MaterialApp(
+    title: 'SQLite',
+    home: Home(),
+  ));
+}
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SQLite'),
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+      ),
+    );
+  }
+}
